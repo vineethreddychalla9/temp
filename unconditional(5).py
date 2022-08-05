0a69d09c9c94973bc442a368e864cd945578f193
Vineeth Reddy <62389014+vineethreddychalla9@users.noreply.github.com>
2022-08-05 10:01:15 +0530

    Update unconditional(5).py

--- unconditional(5).py
+++ unconditional(5).py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_csv("individual (1).csv")
+df = pd.read_csv("https://raw.githubusercontent.com/KMITDS/CS601PC/main/individual.csv")
 print(df.head(10))
 print("P(recreation=golf)=",df[(df.recreation=='golf')].shape[0] / df.shape[0])
 res = df[(df.status=="single") & (df.cw=="medRisk")].shape[0] / df[(df.cw=="medRisk")].shape[0]
